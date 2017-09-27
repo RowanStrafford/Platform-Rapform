2a2e3968d99050d100920a2f39dcd39b61d9d5bb
RowanStrafford <rowan.strafford@ymail.com>
2017-09-27 18:14:44 +0100

    Setup GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject platform;
+    float spawnSeconds = 3f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnPlatform", 0, spawnSeconds);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void SpawnPlatform()
+    {
+        Instantiate(platform, new Vector3(Random.Range(-3.5f, 3.5f), 7.5f, 0), Quaternion.identity);
+    }
+}
